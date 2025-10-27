035aa8966134a57438bfc8341e6918ee5b0fd9d9
luiz-chiq <chiquetanoluiz@gmail.com>
2025-10-26 22:02:12 -0300

    feat: add zustend to the project

---
+++ src/hooks/zuntand/useBoard.ts
@@ -0,0 +1,34 @@
+// store/useBear.ts
+import { create } from "zustand";
+import { Color, type Board, type BoardPosition } from "../../types/types";
+import { CreateBoardService } from "../../service/board/createBoardService";
+
+type BoardStore = {
+  board: Board | null;
+  clickedSquare: BoardPosition | null;
+  turn: Color;
+  player: Color;
+  setClickedSquare: (square: BoardPosition) => void;
+  passTurn: () => void;
+  changePlayer: () => void;
+};
+
+export const useBoard = create<BoardStore>()((set) => ({
+  board: CreateBoardService.createBoard(),
+  clickedSquare: null,
+  turn: Color.WHITE,
+  player: Color.WHITE,
+  setClickedSquare: (position) => {
+    set({ clickedSquare: position });
+  },
+  passTurn: () => {
+    set((state) => ({
+      turn: state.turn === Color.WHITE ? Color.BLACK : Color.WHITE,
+    }));
+  },
+  changePlayer: () => {
+    set((state) => ({
+      player: state.player === Color.WHITE ? Color.BLACK : Color.WHITE,
+    }));
+  },
+}));
