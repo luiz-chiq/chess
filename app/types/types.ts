eb21db090439f8f50ebe4ecf3837e608bc473ad6
luiz-chiq <chiquetanoluiz@gmail.com>
2025-11-13 00:02:04 -0300

    feat: remove unused types

--- app/types/types.ts
+++ app/types/types.ts
@@ -4,10 +4,7 @@ export enum Color {
 }
 export type BoardRow = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 export type BoardCollumn = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H";
-export type NewBoardRow = "12345678"
-export type NewBoardCollumn = "ABCDEFGH"
 export type BoardPosition = `${BoardCollumn}${BoardRow}`;
-export type Board = Record<BoardPosition, Square>;
 export type Square = {
   position: BoardPosition;
   color: Color;
