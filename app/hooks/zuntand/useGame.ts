bb6b8bd401790da230f92c35c522147385e501dd
luiz-chiq <chiquetanoluiz@gmail.com>
2025-11-12 23:59:08 -0300

    refactor: change how application is using zustand

---
+++ app/hooks/zuntand/useGame.ts
@@ -0,0 +1,52 @@
+import { Board } from '@/app/types/Board'
+import { BoardPosition, Color, possibleMove } from '@/app/types/types'
+import { create } from 'zustand'
+
+interface GameState {
+  board: Board
+  player: Color
+  clickedPiece: BoardPosition | null
+  turn: Color
+  possibleMoves: possibleMove[]
+  movePiece: (from:BoardPosition, to: BoardPosition) => void
+  setClickedPiece: (piecePosition: BoardPosition) => void
+  unsetClickedPiece: () => void
+  passTurn: () => void
+  setPossibleMoves: (possibleMoves: possibleMove[]) => void
+}
+
+
+export const useGame = create<GameState>()(
+    (set) => ({
+      board: new Board(),
+      player: Color.WHITE,
+      clickedPiece: null,
+      turn: Color.WHITE,
+      possibleMoves: [],
+      movePiece: (from, to) =>
+        set((state) => {
+          state.board.movePiece(from, to);
+          return { board: state.board };
+        }),
+      setClickedPiece: (piecePosition) => {
+        set((state) => ({
+          clickedPiece: piecePosition
+        }))
+      },
+      unsetClickedPiece: () => {
+        set((state) => ({
+          clickedPiece: null
+        }))
+      },
+      passTurn: () => set((state) => ({
+         turn : state.turn == Color.WHITE ? Color.BLACK : Color.WHITE,
+         clickedPiece: null,
+         possibleMoves: []
+      })),
+      setPossibleMoves: (possibleMoves) => {
+        set((state) => ({
+          possibleMoves: possibleMoves
+        }))
+      }
+    }),
+)
