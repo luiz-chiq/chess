f389da6c7445ee797f7fb8511f57441de0c249df
luiz-chiq <chiquetanoluiz@gmail.com>
2025-11-13 00:03:38 -0300

    feat: update page.tsx with the new zustand hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,19 @@
 'use client';
 
 import Board from "./components/board/Board";
-import Square from "./components/board/Square";
-import { useBoard } from "./hooks/zuntand/useBoard";
-import { PieceType } from "./types/types";
+import { useGame } from "./hooks/zuntand/useGame";
 
 export default function Page() {
-  const passTurn = useBoard((state) => state.passTurn);
-  const changePlayer = useBoard((state) => state.changePlayer);
-  const turn = useBoard((state) => state.turn);
-  const player = useBoard((state) => state.player);
+  const passTurn = useGame((state) => state.passTurn);
+  const turn = useGame((state) => state.turn);
   return (
     <>
       <div className="boardHead">
         <div>
           <p>Current turn:</p>
           <img src={`/assets/pieces/${turn}/king.svg`}/>
-          {/* <button onClick={passTurn}>pass turn</button> */}
+          <button onClick={passTurn}>pass turn</button>
         </div>
-        {/* <div>
-          <p>You:</p>
-          <img src={`/assets/pieces/${player}/king.svg`}/>
-          <button onClick={changePlayer}>change player</button>
-        </div> */}
       </div>
       <Board />
     </>
