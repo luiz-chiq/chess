49410480336fefb11ceb4b7cb24a9a8cb81a4136
luiz-chiq <chiquetanoluiz@gmail.com>
2025-11-12 23:59:36 -0300

    feat: create utils.ts

---
+++ app/utils/utils.ts
@@ -0,0 +1,21 @@
+import { BoardCollumn, BoardPosition, BoardRow } from "../types/types";
+
+export function getRow(index: number): BoardRow {
+    return (index + 1) as BoardRow;
+}
+    
+export function getCol(index: number): BoardCollumn {
+    return String.fromCharCode(65 + index) as BoardCollumn;
+}
+
+export function getBoardPositionByIndexes(row: number, col: number): BoardPosition {
+    return getCol(col) + getRow(row) as BoardPosition;
+}
+
+export function getRowIndex(position: BoardPosition): number {
+    return parseInt(position[1]) - 1;
+}
+
+export function getColIndex(position: BoardPosition): number {
+    return position.charCodeAt(0) - 65;
+}
