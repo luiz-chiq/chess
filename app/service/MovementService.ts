503b12decb4bf28d2be033f368a98f6c6b2d9a67
luiz-chiq <chiquetanoluiz@gmail.com>
2025-11-13 00:09:05 -0300

    feat: create pawn moveset

--- app/service/MovementService.ts
+++ app/service/MovementService.ts
@@ -1,36 +1,39 @@
+import { Board } from "../types/Board";
 import {
   Color,
   PieceType,
-  possibleMoves,
-  type Board,
+  PossibleMove,
   type BoardPosition,
   type Piece,
 } from "../types/types";
+import { getColIndex, getRowIndex, getBoardPositionByIndexes, getRow, getCol } from "../utils/utils";
 
 export class MovementService {
 
   static getPossibleMoves(
     position: BoardPosition,
     board: Board
-  ): possibleMoves {
+  ): PossibleMove[] {
 
-    if (!board[position].piece) return { moveset: [], piecesToCapture: [] };
+    const piece = board.getPiece(position)
 
-    switch (board[position].piece.type) {
+    if (piece == null) return [];
+
+    switch (piece.type) {
         case PieceType.PAWN:
-            return this.getPawnMoveset(position, board);
-        case PieceType.ROOK:
-            return this.getRookMoveset(position, board);
+            return this.getPawnMoveset(piece, position, board);
+        // case PieceType.ROOK:
+        //     return this.getRookMoveset(piece, position, board);
         // case PieceType.KNIGHT:
-        //     return this.getKnightMoveset(position, board);
+        //     return this.getKnightMoveset(piece, position, board);
         // case PieceType.BISHOP:
-        //     return this.getBishopMoveset(position, board);
+        //     return this.getBishopMoveset(piece, position, board);
         // case PieceType.QUEEN:
-        //     return this.getQueenMoveset(position, board);
+        //     return this.getQueenMoveset(piece, position, board);
         // case PieceType.KING:
-            // return this.getKingMoveset(position, board);
+            // return this.getKingMoveset(pieace, position, board);
         default:
-            return { moveset: [], piecesToCapture: [] };
+            return [];
     }
   }
 
@@ -40,63 +43,60 @@ export class MovementService {
     board: Board
   ): Board {
 
-    if (board[piecePosition].piece) {
-
-      board[newPosition].piece = board[piecePosition].piece
-      board[piecePosition].piece = null;
-    }
+    if (board.getPiece(piecePosition)) board.movePiece(piecePosition, newPosition)
 
     return board
   }
 
   private static getPawnMoveset(
+    piece: Piece,
     position: BoardPosition,
     board: Board
-  ): possibleMoves {
-    const possibleMoves: possibleMoves = {
-      moveset: [],
-      piecesToCapture: []
-    };
-    const piece = board[position].piece;
-    if (!piece) return possibleMoves;
-    
+  ): PossibleMove[] {
+    const possibleMoves: PossibleMove[] = [];
+
+    const rowIndex = getRowIndex(position);
+    const colIndex = getColIndex(position);
+    const oppositeColor = piece.color === Color.WHITE ? Color.BLACK : Color.WHITE;
+    const rightAvailable = colIndex < 7;
+    const leftAvailable = colIndex > 0;    
     const direction = piece.color === Color.WHITE ? 1 : -1;
-    const isFirstMove = piece.color === Color.WHITE && position[1] == "2" || Color.WHITE && position[1] == "7";
-    const isNextSquareAvailable: boolean = parseInt(position[1]) < 8 && board[`${position[0]}${parseInt(position[1]) + 1 * direction}` as BoardPosition].piece == null;
-    const isSecondSquareAvaliable: boolean = isNextSquareAvailable && isFirstMove && board[`${position[0]}${parseInt(position[1]) + 2 * direction}` as BoardPosition].piece == null;
+    const chessBoard = board.getBoard();
+
+    const isFirstMove = 
+      (piece.color === Color.WHITE && rowIndex == 1) ||
+       piece.color === Color.BLACK && rowIndex == 6;
+    const isNextSquareAvailable: boolean = 
+      rowIndex < 8 &&
+      board.getPiece(getBoardPositionByIndexes(rowIndex + direction, colIndex)) === null
+    const isSecondSquareAvaliable: boolean =
+      isFirstMove &&
+      isNextSquareAvailable &&
+      board.getPiece(getBoardPositionByIndexes(rowIndex + (direction * 2), colIndex)) === null
     
-    if (isNextSquareAvailable)
-      possibleMoves.moveset.push(`${position[0]}${parseInt(position[1]) + 1 * direction}` as BoardPosition);
+    if (isNextSquareAvailable) {
+      const movePosition = getBoardPositionByIndexes(rowIndex + (1 * direction), colIndex)
+      possibleMoves.push({ newPosition: movePosition, targetPiece: movePosition });
+    }
 
-    if (isFirstMove && isSecondSquareAvaliable)
-      possibleMoves.moveset.push(`${position[0]}${parseInt(position[1]) + 2 * direction}` as BoardPosition);
+    if (isSecondSquareAvaliable) {
+      const movePosition = getBoardPositionByIndexes(rowIndex + (2 * direction), colIndex)
+      possibleMoves.push({ newPosition: movePosition, targetPiece: movePosition });
+    }
+    const canTakeRightDiagonal = rightAvailable && 
+      chessBoard[rowIndex + direction][colIndex + 1].piece?.color === oppositeColor;
+    const canTakeLeftDiagonal = leftAvailable && 
+      chessBoard[rowIndex + direction][colIndex - 1].piece?.color === oppositeColor;
 
-
+    if (canTakeRightDiagonal) {
+      const movePosition = getBoardPositionByIndexes(rowIndex + direction, colIndex + 1)
+      possibleMoves.push({ newPosition: movePosition, targetPiece: movePosition });
+    }
+    if (canTakeLeftDiagonal) {
+      const movePosition = getBoardPositionByIndexes(rowIndex + direction, colIndex - 1)
+      possibleMoves.push({ newPosition: movePosition, targetPiece: movePosition });
+    }
     return possibleMoves;
   }
-  private static getRookMoveset(
-    position: BoardPosition,
-    board: Board
-  ): possibleMoves {
-    const possibleMoves: possibleMoves = {
-      moveset: [],
-      piecesToCapture: []
-    };
-    const piece = board[position].piece;
-    if (!piece) return possibleMoves;
-    
-    const direction = piece.color === Color.WHITE ? 1 : -1;
-    const isFirstMove = piece.color === Color.WHITE && position[1] == "2" || Color.WHITE && position[1] == "7";
-    const isNextSquareAvailable: boolean = parseInt(position[1]) < 8 && board[`${position[0]}${parseInt(position[1]) + 1 * direction}` as BoardPosition].piece == null;
-    const isSecondSquareAvaliable: boolean = isNextSquareAvailable && isFirstMove && board[`${position[0]}${parseInt(position[1]) + 2 * direction}` as BoardPosition].piece == null;
-    
-    if (isNextSquareAvailable)
-      possibleMoves.moveset.push(`${position[0]}${parseInt(position[1]) + 1 * direction}` as BoardPosition);
 
-    if (isFirstMove && isSecondSquareAvaliable)
-      possibleMoves.moveset.push(`${position[0]}${parseInt(position[1]) + 2 * direction}` as BoardPosition);
-
-
-    return possibleMoves;
-  }
 }
