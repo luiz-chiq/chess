4faee0f65a11995b0f17e3f5ff8c82f44a0fde6f
luiz-chiq <chiquetanoluiz@gmail.com>
2025-11-23 03:54:31 -0300

    refactor: simplifying piece movement logic

--- app/src/components/board/piece/Piece.tsx
+++ app/src/components/board/piece/Piece.tsx
@@ -1,30 +1,20 @@
 'use client';
 
-import { BoardPosition } from "@/app/src/types/types";
-import { useGame } from "../../../hooks/zustand/useGame";
 import { useMemo } from "react";
 import styles from "./styles.module.css";
+import { Piece } from "@/app/src/types/types";
 
-interface PieceProps {
-  position: BoardPosition;
+interface Props {
+  piece: Piece | null;
 }
 
-export default function Piece({ position }: PieceProps) {
-  const turn = useGame((state) => state.turn);
-  const board = useGame((state) => state.board);
-  const piece = board.getPiece(position);
-  const setClickedPiece = useGame((state) => state.setClickedPiece);
-
-  const handleClick = () => position && piece?.color === turn && setClickedPiece(position);
-  
-
+export default function PieceImage({ piece }: Props) {
   return useMemo(() =>
     piece &&
         <img 
-          onClick={handleClick} 
           className={styles.piece}
           src={`/assets/pieces/${piece.color}/${piece.type}.svg`}
           alt={`${piece.color} ${piece.type}`} 
         />
-    , [piece, handleClick]);
+    , [piece]);
 }
--- app/src/components/board/square/Square.tsx
+++ app/src/components/board/square/Square.tsx
@@ -2,8 +2,9 @@
 
 import { BoardPosition, Color } from "@/app/src/types/types";
 import { useGame } from "../../../hooks/zustand/useGame";
-import SquareContent from "./SquareContent";
 import styles from "./styles.module.css";
+import PieceImage from "../piece/Piece";
+import { useCallback, useEffect, useMemo } from "react";
 
 interface SquareProps {
   color: Color;
@@ -14,14 +15,46 @@ function Square({ position, color }: SquareProps) {
   const clickedPiecePosition = useGame((state) => state.clickedPiecePosition);
   const turn = useGame((state) => state.turn);
   const board = useGame((state) => state.board);
+  const possibleMoves = useGame((state) => state.possibleMoves);
+  
+  const setClickedPiece = useGame((state) => state.setClickedPiece);
+  const movePiece = useGame((state) => state.movePiece);
+  
   const piece = board.getPiece(position);
 
+  const isAllyPiece = useMemo(() => 
+    piece && 
+    piece.color === turn && 
+    clickedPiecePosition !== position,
+  [piece, turn, clickedPiecePosition, position]);
+
+  const destination = useMemo(() => 
+    possibleMoves.get(position) === position,
+  [possibleMoves, position]);
+
+  useEffect(() => {
+    console.log("123 " + possibleMoves.keys().toArray())
+  }, [possibleMoves]);
+
+  const handleClick = useCallback(() => {
+    if (destination)
+      return movePiece(position);
+    if (isAllyPiece) 
+      setClickedPiece(position);
+    {possibleMoves.get(position) === position && <div className={styles.destination}/>}
+  }, [destination, movePiece, position, isAllyPiece, setClickedPiece, possibleMoves]);
+
+
   return (
-    <div className={`${styles.square} ${styles[color]}
+    <div className={`
+      ${styles.square} ${styles[color]}
       ${clickedPiecePosition == position ? styles.clicked : styles.notClicked}
-      ${piece && piece.color == turn && styles.allyPiece}
-      `}>
-        <SquareContent position={position} />
+      ${isAllyPiece && styles.allyPiece}
+      ${destination && piece && styles.checked}
+      `}
+      onClick={handleClick}>
+        <PieceImage piece={piece} />
+        {destination && !piece && <div className={styles.destination}/>}
     </div>
   );
 }
