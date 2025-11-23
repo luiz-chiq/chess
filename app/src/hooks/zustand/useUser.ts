51221d456da4b41be5841e648f2ed7f516516624
luiz-chiq <chiquetanoluiz@gmail.com>
2025-11-23 00:05:52 -0300

    feat: create useUser store

---
+++ app/src/hooks/zustand/useUser.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand'
+
+interface UserState {
+  username: string
+  token: string
+  setUsername: (username: string) => void
+  setToken: (token: string) => void
+}
+
+
+export const useUser = create<UserState>()(
+    (set) => ({
+        username: "",
+        token: "",
+        setToken: (token: string) =>  set(() => ({
+            token
+        })),
+        setUsername: (username: string) => set(() => ({
+            username
+        })),
+    }),
+)
